01786e5cdb3b59c1aa3f6203bc8eb021d6f5ea77
Harish Kumar T <110085393+botkumar01@users.noreply.github.com>
2025-09-23 04:14:22 +0530

    Time: 8 ms (70.27%), Space: 12.4 MB (81.49%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x<0:
+            return False
+        # c = x[
+        # return c
+        return x == int(str(x)[::-1])
