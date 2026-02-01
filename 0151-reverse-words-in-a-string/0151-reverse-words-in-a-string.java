606bf2807b267fc5eb1a0702806a889edc4e411f
Harish Kumar T <110085393+botkumar01@users.noreply.github.com>
2026-02-01 14:15:15 +0530

    Time: 25 ms (5.37%), Space: 44.8 MB (30.13%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,11 +1,47 @@
-import java.util.*;
 class Solution {
     public String reverseWords(String s) {
-        // String chh =(s.trim());
-        String[] ch = s.trim().split("\\s+");
-        // char[] k = ch.toCharArray();
-        Collections.reverse(Arrays.asList(ch));
-        // String l = new String(ch);
-        return String.join(" ",ch);
+        int l=0;
+        StringBuilder one = new StringBuilder();
+        StringBuilder ans = new StringBuilder();
+        for(;l<s.length();){
+            while(l<s.length() && s.charAt(l) == ' '){
+                // System.out.println(one);
+
+                if (one.length() != 0){
+                    if(ans.length() == 0){
+                        System.out.println("d");
+                    ans.insert(0,one);
+                    }
+                    // ans.insert(0," ");
+                    else {
+                                        // System.out.println(one.toString());
+                        System.out.println("d");
+
+                        ans.insert(0," ");
+                        ans.insert(0,one);
+                    
+                    }
+                    
+                    one.setLength(0);
+                }
+                l++;
+            }
+            while(l<s.length() && (s.charAt(l) != ' ')){
+                one.append(s.charAt(l));
+                l++;
+                
+            }
+            // System.out.println(ans);
+            // l++;
+        }
+                        // System.out.println(one);
+
+        if(one.length() !=0){
+            if(ans.length() !=0){
+                ans.insert(0," ");
+            }
+        ans.insert(0,one);
+        }
+        return ans.toString();
     }
 }
